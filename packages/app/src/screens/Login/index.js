ddf2846b7d52f6305dff60c2e1411d4d8012965e
Bob Wei <bob.wei@linecorp.com>
2019-07-20 16:41:30 +0800

    Add login page

---
+++ packages/app/src/screens/Login/index.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-alert */
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+
+import styles from './styles';
+import login from '../../instagram/apis/login';
+
+const Comp = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Input
+        label="Username"
+        containerStyle={styles.section}
+        value={username}
+        onChangeText={setUsername}
+        placeholder="Username"
+        autoCapitalize="none"
+      />
+      <Input
+        label="Password"
+        containerStyle={styles.section}
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <Button
+        style={styles.section}
+        title={!isLoading ? 'Submit' : 'Loading...'}
+        type="solid"
+        onPress={createOnLogin({
+          username,
+          password,
+          navigation,
+          setIsLoading,
+        })}
+      />
+    </View>
+  );
+};
+
+export default Comp;
+
+function createOnLogin({ username, password, navigation, setIsLoading }) {
+  return () => {
+    setIsLoading(true);
+    login({ username, password })
+      .then(() => {
+        setIsLoading(false);
+        navigation.navigate('Home', { isAuthenticated: true });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        alert(error);
+      });
+  };
+}
---
+++ packages/app/src/screens/Login/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    padding: 15,
+  },
+  section: {
+    marginBottom: 20,
+  },
+});
+
+export default styles;
