053d0eeec60979479fedc5c7a854b7bd34121ad6
Bob Wei <bob.wei@linecorp.com>
2019-07-21 11:52:50 +0800

    Rm cache from storage

--- packages/app/src/hooks/usePosts/index.js
+++ packages/app/src/hooks/usePosts/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useEffect, useState } from 'react';
 import * as R from 'ramda';
-import AsyncStorage from '@react-native-community/async-storage';
 
 import getTimeline from '../../instagram/apis/getTimeline';
 import getOwnerTimeline from '../../instagram/apis/getOwnerTimeline';
@@ -24,16 +23,11 @@ export const friendsKey = '@project/friends';
 export const postsKey = '@project/posts';
 
 async function loadData({ setPosts }) {
-  ((await AsyncStorage.getItem(postsKey)) || Promise.resolve('{"data":[]}'))
-    .then((str) => JSON.parse(str))
-    .then((data) => setPosts(data.data));
   const friends = await (async () => {
-    const cached = JSON.parse((await AsyncStorage.getItem(friendsKey)) || '{"data":[]}');
     const latest = await getTimeline()
       .then(R.pathOr([], ['data', 'user', 'edge_web_feed_timeline', 'edges']))
       .then(R.map(R.path(['node', 'owner', 'id'])));
-    const result = [...new Set([...cached.data, ...latest])];
-    AsyncStorage.setItem(friendsKey, JSON.stringify({ data: result }));
+    const result = [...new Set(latest)];
     return result;
   })();
   const posts = [];
